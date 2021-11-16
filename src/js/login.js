0f9d631697b28a588e8c5f85bc9d5f2240f92445
macbook <drivespublic@gmail.com>
2021-11-16 09:10:02 +0900

    accept change

--- src/js/login.js
+++ src/js/login.js
@@ -1,49 +1,22 @@
-<<<<<<< HEAD
 window.addEventListener("DOMContentLoaded", () => {
-  const loginBtnEl = document.getElementById('loginMainBtn')
-  const accountFindEl = document.getElementById('mainAccountFind')
-  const accountRegisterEl = document.getElementById('mainAccountRegister')
+  const loginBtnEl = document.getElementById("loginMainBtn");
+  const accountFindEl = document.getElementById("mainAccountFind");
+  const accountRegisterEl = document.getElementById("mainAccountRegister");
 
   const loginBtnClickHandler = (e) => {
     // login 검증
-     console.log(e.target)
-  }
+    console.log(e.target);
+  };
 
   const accountFindClickHandler = (e) => {
-    window.location.href = "/account/find"
-  }
+    window.location.href = "/account/find";
+  };
 
   const accountRegisterClickHandler = (e) => {
-    window.location.href = "/account/register"
- }
+    window.location.href = "/account/register";
+  };
 
-
-  loginBtnEl.addEventListener("click", loginBtnClickHandler)
-  accountFindEl.addEventListener('click', accountFindClickHandler)
-  accountRegisterEl.addEventListener('click', accountRegisterClickHandler)
+  loginBtnEl.addEventListener("click", loginBtnClickHandler);
+  accountFindEl.addEventListener("click", accountFindClickHandler);
+  accountRegisterEl.addEventListener("click", accountRegisterClickHandler);
 });
-=======
-window.addEventListener("DOMContentLoaded", () => {
-  const loginBtnEl = document.getElementById('loginMainBtn')
-  const accountFindEl = document.getElementById('mainAccountFind')
-  const accountRegisterEl = document.getElementById('mainAccountRegister')
-
-  const loginBtnClickHandler = (e) => {
-    // login 검증
-     console.log(e.target)
-  }
-
-  const accountFindClickHandler = (e) => {
-    window.location.href = "/account/find"
-  }
-
-  const accountRegisterClickHandler = (e) => {
-    window.location.href = "/account/register"
- }
-
-
-  loginBtnEl.addEventListener("click", loginBtnClickHandler)
-  accountFindEl.addEventListener('click', accountFindClickHandler)
-  accountRegisterEl.addEventListener('click', accountRegisterClickHandler)
-});
->>>>>>> 155242d9770d2fdbe12fb78188112dd009364c47
